7e0ad125597360871e6196506307e9d7753fc1c6
venkatesh1290 <venkateshmeruva04@gmail.com>
2025-06-11 20:26:30 +0530

    oops concepts

--- python_sample_file2.py
+++ python_sample_file2.py
@@ -1,1 +1,16 @@
-print("Venkat World!")
+class Bankaccount():
+    def __init__(self,balance,credit):
+        self.balance = balance 
+        self.credit = credit 
+    def get_adding_amount(self,amount):
+        self.balance = self.balance + amount 
+    def get_credit_amount(self):
+        return self.credit  
+    def get_total_amount(self):
+        return self.balance + self.credit 
+
+get_result = Bankaccount(1000,2000) 
+get_result.get_adding_amount(8000) 
+print(get_result.balance)
+print(get_result.get_credit_amount())
+print(get_result.get_total_amount())
